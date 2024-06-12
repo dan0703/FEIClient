6903a1cc5d65299765c097bd6d7982219ef69d9a
dan0703 <dansegura8863@gmail.com>
2024-06-11 18:43:55 -0600

    Ventana de Inicio de sesion creada

---
+++ FEIClient/Views/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FEIClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ShowPassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_SignUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
